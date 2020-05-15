a3c080a9e718f8441eddcc134a7b5e6d1fdca0b8
dengxiaobing <657563945dxb@gmail.com>
2020-05-15 10:46:55 +0800

    开始学习Java多线程

---
+++ src/main/java/chapter01/section02/Test.java
@@ -0,0 +1,12 @@
+package chapter01.section02;
+
+/**
+ * @author: deng
+ * @datetime: 2020/5/15 10:39 上午
+ * @desc:
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(Thread.currentThread().getName());
+    }
+}
